9e4b9f629b6a1adb8d01ba6670b15194912d83ac
Khaled-Khlifi2 <khaledkhlifi41@gmail.com>
2025-10-17 20:30:52 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+
+const navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default navbar
