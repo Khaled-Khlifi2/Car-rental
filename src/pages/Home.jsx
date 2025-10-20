fa2d6a59c09c78d39ed390e9997deafe18651f60
Khaled-Khlifi2 <khaledkhlifi41@gmail.com>
2025-10-20 10:56:25 +0100

    home page setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import Banner from "../components/Banner"
+import FeaturedSection from "../components/FeaturedSection"
 import HomeSection1 from "../components/HomeSection1"
+import Testimonial from "../components/Testimonial"
 
 
 const Home = () => {
   return (
     <>
       <HomeSection1 />
+      <FeaturedSection />
+      <Banner/>
+      <Testimonial />
     </>
   )
 }
