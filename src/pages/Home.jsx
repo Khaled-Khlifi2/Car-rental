ca90d209db7e8e3f49c4562565a0ef8de8a26640
Khaled-Khlifi2 <khaledkhlifi086@gmail.com>
2025-10-20 22:43:17 +0100

    footer and newsletter implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import HomeSection1 from "../components/HomeSection1"
+import HomeSection1 from "../components/HomeSection1";
+import FeaturedSection from "../components/FeaturedSection";
+import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
+import Newsletter from "../components/Newsletter";
 
 
 const Home = () => {
   return (
     <>
       <HomeSection1 />
+      <FeaturedSection />
+      <Banner/>
+      <Testimonial/>
+      <Newsletter/>
+
     </>
   )
 }
